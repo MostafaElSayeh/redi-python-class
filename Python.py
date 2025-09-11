f0fd92f1d80ece6ed2ef778cdd53f7032c4d10d8
MostafaElSayeh <eng.mostufa88@gmail.com>
2025-09-11 20:46:36 +0300

    first commit

---
+++ Python.py
@@ -0,0 +1,9 @@
+print("***** Hello, Redi! *****\n") # Welcome Msg
+
+user_name = input("Enter Your Name: ")
+user_age = int(input("Enter Your Age: "))
+user_country = input("Where are you from? ")
+
+print(f"Hello, {user_name}! \
+You are {user_age} years old, \
+and You are from {user_country}")
