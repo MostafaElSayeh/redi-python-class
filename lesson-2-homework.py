d3384634088c0bda7bda43fe4c73df47412c1074
MostafaElSayeh <123393241+MostafaElSayeh@users.noreply.github.com>
2025-09-16 15:30:45 +0300

    Create lesson-2-homework.py

---
+++ lesson-2-homework.py
@@ -0,0 +1,49 @@
+# Write a program that will do the following:
+# 1. Continuously loop until the user enters the empty string. They can do
+#    this by simply pressing Enter or Return without entering in any
+#    characters.
+# 2. Within the loop, ask the user to enter a string. Again, if they just 
+#    press the Enter or Return key, this results in an "empty string". 
+#    If they entered the empty string, end the loop. The empty string
+#    in code is "". Otherwise...
+while True:
+    user_input = input("Enter Your String (Press ENTER to quit): ")
+
+    if user_input == "":
+        print("Program Ends...")
+        break
+# 3. Print out four different versions of the string using various string 
+#    functions:
+#       a) Print out the string with only the first letter capitalized. 
+#       b) Print out the string in all lowercase.
+#       c) Print out the string in title case. 
+#       d) Print out the string in all uppercase.
+    else:
+        print("\nYour String:")
+        print("a) Capitalized: ", user_input.capitalize())
+        print("b) Lowercase:   ", user_input.lower())
+        print("c) Title Case:  ", user_input.title())
+        print("d) Uppercase:   ", user_input.upper())
+# 4. Then loop around and ask the user for another string, following the 
+#    logic above.
+#
+# Here is how the output might look like, but feel free to experiment with
+# your code:
+#
+#   Enter a string: <user presses Enter>
+#   Thanks for playing! <and the program ends>
+#
+# <Run the program again>
+#
+#   Enter a string: This Is a STRING with MiXeD casing.
+#   First letter capitalized: This is a string with mixed casing.
+#   All lowercase:            this is a string with mixed casing.
+#   Title case:               This Is A String With Mixed Casing.
+#   All uppercase:            THIS IS A STRING WITH MIXED CASING.
+#   Enter a string: this is ANOTHER string with mixed casing.
+#   First letter capitalized: This is another string with mixed casing.
+#   All lowercase:            this is another string with mixed casing.
+#   Title case:               This Is Another String With Mixed Casing.
+#   All uppercase:            THIS IS ANOTHER STRING WITH MIXED CASING. 
+#   Enter a string: <user presses Enter>
+#   Thanks for playing! <and the program ends>
